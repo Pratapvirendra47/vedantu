733f7002f557a13c47101a8b975b48c643905a1b
Virendra Pratap <virendra.pratap@homeyantra.com>
2020-07-28 14:50:41 +0530

    vedantu assignment

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -4,65 +4,112 @@ import './dashboardStyle.css';
 
 const Dashboard = () => {
 
+  const [searchVariable, setSearchVariable] = useState("");
+
   useEffect(() => {
     console.log(GitHubData.userData.login)
 
   }, [])
 
+  const setSearch = (e) => {
+    setSearchVariable(e.target.value);
+  }
+
   return (
     <div className="container">
       {/* header */}
-      <div className="githubHeader">
-        <div className="afterImageHeader">
-        </div>
-        <div className="contentHeader">
-          <div className="overview">Overview</div>
-          <div className="repositories">Repositories {GitHubData.userData.public_repos}</div> 
-          <div className="projects">Projects</div>
-        </div>
-      </div>
-      <div className="mainContentSection">
-        {/* left section */}
-        <div className="leftIntroSection">
-          <div>
-            <img src = {GitHubData.userData.avatar_url} />
+      <div>
+        {/* <div className="githubHeader">
+          <div className="afterImageHeader">
           </div>
-          <div className="namingCredentials">
-            <h2>{GitHubData.userData.name}</h2>
-            <h3>{GitHubData.userData.login}</h3>
+          <div className="contentHeader">
+            <div className="overview">Overview</div>
+            <div className="repositories">Repositories {GitHubData.userData.public_repos}</div> 
+            <div className="projects">Projects</div>
           </div>
-          <div className="credentialsDiv" >
-            <p className="bioCredentials">{GitHubData.userData.bio}</p>
+        </div> */}
+        <div className="mainContentSection">
+          {/* left section */}
+          <div className="githubHeader">
+            <div className="afterImageHeader">
+            </div>
+            <div className="contentHeader">
+              <div className="overview">Overview</div>
+              <div className="repositories">Repositories {GitHubData.userData.public_repos}</div> 
+              <div className="projects">Projects</div>
+            </div>
           </div>
-          <div className="followDesign">Follow</div>
-          <div className="followersDiv">
-            <div><b>{GitHubData.userData.followers}</b><span> Followers </span></div>
-            <div><b>{GitHubData.userData.following}</b> <span>Following</span></div>
-          </div>
-          <p className="companyPara">{GitHubData.userData.company}</p>
-          <p className="locationPara">{GitHubData.userData.location}</p>
-        </div>
-        {/* Right Section  */}
-        <div className="rightDetailSection">
-          <div>
-              <input placeholder="Find a repository"/>
-              <select></select>
-              <select></select>
-          </div>
-          <div>
+          <div className="leftIntroSection">
             <div>
-              {
-                GitHubData.repoData.map((repoValue,index) => {
-                  return (
-                    <div>
-                      <h3>{repoValue.name}</h3>
-                      <p>{repoValue.description}</p>
-                      <p>{repoValue.language}</p>
-                      <p>Updated on {repoValue.updated_at}</p>
-                    </div>
-                  )
-                })
-              }
+              <img src = {GitHubData.userData.avatar_url} />
+            </div>
+            <div className="namingCredentials">
+              <h2>{GitHubData.userData.name}</h2>
+              <h3>{GitHubData.userData.login}</h3>
+            </div>
+            <div className="credentialsDiv" >
+              <p className="bioCredentials">{GitHubData.userData.bio}</p>
+            </div>
+            <div className="followDesign">Follow</div>
+            <div className="followersDiv">
+              <div><b>{GitHubData.userData.followers}</b><span> Followers </span></div>
+              <div><b>{GitHubData.userData.following}</b> <span>Following</span></div>
+            </div>
+            <p className="companyPara">{GitHubData.userData.company}</p>
+            <p className="locationPara">{GitHubData.userData.location}</p>
+          </div>
+          {/* Right Section  */}
+          <div className="rightDetailSection">
+            <div className="searchSection">
+                <div className="searchBox">
+                  <input className="inputSearch" placeholder="Find a repository" onChange={(e) => {setSearch(e)}}/>
+                </div>
+                <div className="selectionType">
+                  <select className="SelectionTag">
+                    <option> Select Type</option>
+                    <option> All </option>  
+                    <option> Sources </option>
+                    <option> Forked </option>
+                    <option> Archived </option>
+                    <option> Mirror </option>
+                  </select>
+                </div>
+                <div className="selectLanguage">
+                  <select className="languageTag">
+                    <option>All</option>
+                  </select>
+                </div>
+            </div>
+            <div>
+              <div>
+                {
+                  GitHubData.repoData.map((repoValue,index) => {
+                    return (
+                      // <div key = {index}>
+                        // {
+                          // searchVariable != null ?
+                            // searchVariable == repoValue.name ?
+                              <div key= {index} className="repoDataStyling">
+                                <h2 className="repoValueName">{repoValue.name}</h2>
+                                <p className="repoDescription">{repoValue.description}</p>
+                                <p className="repoLanguage">{repoValue.language}</p>
+                                <p className="repoUpdatedStatus">Updated on {repoValue.updated_at}</p>
+                              </div>
+                              // :
+                        //       <div className="repoDataStyling">
+                        //         <h2 className="repoValueName">{repoValue.name}</h2>
+                        //         <p className="repoDescription">{repoValue.description}</p>
+                        //         <p className="repoLanguage">{repoValue.language}</p>
+                        //         <p className="repoUpdatedStatus">Updated on {repoValue.updated_at}</p>
+                        //       </div>:
+                        //   null
+                        // }
+                        
+                      // </div>
+                    )
+                  })
+                }
+              </div>
             </div>
           </div>
         </div>
